a40b7c87c2763149bfa8a68f369316d96e30ddf8
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-01-16 22:54:59 -0500

    Adding products to backend completed

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -166,6 +166,18 @@ exports.getAllProducts = (req,res)=>{
 };
 
 
+exports.getAllUniqueCategories = (req,res)=>{
+    Product.distinct("category",{},(err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No category found"
+            })
+        }
+        res.json(category)
+    })
+}
+
+
 exports.updateStock = (req,res,next) =>{
 
     //map loops through all the products
