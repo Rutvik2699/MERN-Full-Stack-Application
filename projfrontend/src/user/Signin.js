e1ae2ff77f19b9d38a095d218dba820d10a3673b
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-02-03 19:37:54 -0500

    Making SIgnout work

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
-import { Link,Redirect } from "react-router-dom";
+import {Redirect } from "react-router-dom";
 
 import {signin,authenticate,isAuthenticated} from "../auth/helper"
 
 const Signin = () => {
 
   const [values,setValues] = useState({
-    email:"",
-    password:"",
+    email:"a@hitesh.com",
+    password:"12345",
     error:"",
     loading:false,
     didRedirect:false,
@@ -89,12 +89,12 @@ const handleChange = name => event => {
           <form>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input onChange={handleChange=("email")} value={email} className="form-control" type="email" />
+              <input onChange={handleChange("email")} value={email} className="form-control" type="email" />
             </div>
 
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input onChange={handleChange=("password")} value={email} className="form-control" type="password" />
+              <input onChange={handleChange("password")} value={email} className="form-control" type="password" />
             </div>
             <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
           </form>
