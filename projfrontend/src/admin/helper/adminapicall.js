089d105e505d4a3e66a2310dd334bbcb3c3a9ea7
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-02-05 13:26:11 -0500

    Admin api calls completed

--- projfrontend/src/admin/helper/adminapicall.js
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -1,5 +1,7 @@
 import { API } from "../../backend";
 
+
+//category calls
 export const createCategory = (userId,token,category)=>{
     return fetch(`${API}/category/create/${userId}`,{
         method:"POST",
@@ -15,4 +17,85 @@ export const createCategory = (userId,token,category)=>{
     })
 
     .catch(err=>console.log(err));
+}
+//get all categories
+
+export const getCategories=()=>{
+    return fetch(`${API}/categories`,{
+        method:"GET"
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
+
+//products calls
+//Create a product
+export const createaProduct = (userId,token,product)=>{
+    return fetch(`${API}/product/create/${userId}`,{
+        headers:{
+            Accept:"application/json",
+            Authorization:`Bearer ${token}`
+        },
+        body: product
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
+
+
+//Get all products
+export const getProducts=()=>{
+    return fetch(`${API}/products`,{
+        method:"GET"
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
+
+
+//Delete a product
+
+export const deleteProduct = (productId,userId,token)=>{
+    return fetch(`${API}/product/${productId}/${userId}`,{
+        method:"DELETE",
+        headers:{
+            Accept:"application/json",
+            Authorization:`Bearer ${token}`
+        }
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
+
+
+
+
+//get a product
+export const getProduct = productId=>{
+    return fetch(`${API}/product/${productId}`,{
+        method:"GET"
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
+}
+
+
+//update a product
+export const updateProduct = (productId,userId,token,product)=>{
+    return fetch(`${API}/product/${productId}/${userId}`,{
+        method:"PUT",
+        headers:{
+            Accept:"application/json",
+            Authorization:`Bearer ${token}`
+        },
+        body: product
+    }).then(response=>{
+        return response.json()
+    })
+    .catch(err=>console.log(err))
 }
